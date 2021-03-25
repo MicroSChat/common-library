e30b8946d064ad9f2b04516be39835d4f5e5edc8
Konstantin Buinak <kbuinak@gmail.com>
2021-03-25 19:46:23 +0100

    feat: serializable UserInformationMessage

--- src/main/java/com/microschat/commonlibrary/UserInformationMessage.java
+++ src/main/java/com/microschat/commonlibrary/UserInformationMessage.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
 @Builder
 @AllArgsConstructor
-public class UserInformationMessage {
+public class UserInformationMessage implements Serializable {
     private String username;
     private String nickname;
     private String email;
