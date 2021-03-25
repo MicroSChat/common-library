e1c30a39a7acc0c4739d1790e0081d1f0aced544
Konstantin Buinak <kbuinak@gmail.com>
2021-03-25 19:19:36 +0100

    feat: ConnectivityConstant

---
+++ src/main/java/com/microschat/commonlibrary/connectivity/ConnectivityConstant.java
@@ -0,0 +1,9 @@
+package com.microschat.commonlibrary.connectivity;
+
+public final class ConnectivityConstant {
+    private final static String APPLICATION_EXCHANGE = "msc-exchange";
+
+    private final static String REGISTRATION_USER_ROUTING_KEY = "user.register";
+    private final static String REGISTRATION_USER_AUTH_QUEUE_NAME = "user-register-auth";
+    private final static String REGISTRATION_USER_PDS_QUEUE_NAME = "user-register-pds";
+}
