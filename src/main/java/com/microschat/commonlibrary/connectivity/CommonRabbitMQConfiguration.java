fb0e54acda2f5d011e3351e858775d8313fe974f
Konstantin Buinak <kbuinak@gmail.com>
2021-03-26 20:02:48 +0100

    feat: CommonRabbitMQConfiguration

---
+++ src/main/java/com/microschat/commonlibrary/connectivity/CommonRabbitMQConfiguration.java
@@ -0,0 +1,39 @@
+package com.microschat.commonlibrary.connectivity;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
+
+@Configuration
+public class CommonRabbitMQConfiguration implements RabbitListenerConfigurer {
+
+    @Autowired
+    public ConnectionFactory connectionFactory;
+
+    @Override
+    public void configureRabbitListeners(RabbitListenerEndpointRegistrar rabbitListenerEndpointRegistrar) {
+        rabbitListenerEndpointRegistrar.setMessageHandlerMethodFactory(myHandlerMethodFactory());
+    }
+
+    @Bean
+    public DefaultMessageHandlerMethodFactory myHandlerMethodFactory() {
+        DefaultMessageHandlerMethodFactory factory = new DefaultMessageHandlerMethodFactory();
+        factory.setMessageConverter(new MappingJackson2MessageConverter());
+        return factory;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory() {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setConcurrentConsumers(3);
+        factory.setMaxConcurrentConsumers(10);
+        return factory;
+    }
+}
--- src/main/java/com/microschat/commonlibrary/connectivity/ConnectivityConstant.java
+++ src/main/java/com/microschat/commonlibrary/connectivity/ConnectivityConstant.java
@@ -7,6 +7,4 @@ public final class ConnectivityConstant {
     public final static String APPLICATION_EXCHANGE = "msc-exchange";
 
     public final static String REGISTRATION_USER_ROUTING_KEY = "user.register";
-    public final static String REGISTRATION_USER_AUTH_QUEUE_NAME = "user-register-auth";
-    public final static String REGISTRATION_USER_PDS_QUEUE_NAME = "user-register-pds";
 }
