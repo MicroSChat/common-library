e9c1f820b1c3ce98cabf93776f54a5452a07ccdf
Konstantin Buinak <kbuinak@gmail.com>
2021-03-27 15:27:52 +0100

    feat: user inquiry routing key

--- src/main/java/com/microschat/commonlibrary/connectivity/ConnectivityConstant.java
+++ src/main/java/com/microschat/commonlibrary/connectivity/ConnectivityConstant.java
@@ -8,5 +8,7 @@ public final class ConnectivityConstant {
 
     public final static String USER_REGISTRATION_ROUTING_KEY = "user.register";
     public final static String USER_AUTHENTICATION_ROUTING_KEY = "user.authenticate";
+    public final static String USER_INQUIRY_ROUTING_KEY = "user.inquiry";
+
     public final static String AUTHENTICATION_TOKEN_VALIDATION_ROUTING_KEY = "token.authenticate";
 }
